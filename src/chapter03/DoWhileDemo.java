f54a309d8d3ea9c20465dbde656ba238dd00279b
JuWon <tlswndnjs93@naver.com>
2022-06-21 14:15:16 +0900

    쉽게배우는자바프로그래밍Chapter03

---
+++ src/chapter03/DoWhileDemo.java
@@ -0,0 +1,11 @@
+package chapter03;
+
+public class DoWhileDemo {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.print(i);
+            i++;
+        } while(i<5);
+    }
+}
---
+++ src/chapter03/For1Demo.java
@@ -0,0 +1,9 @@
+package chapter03;
+
+public class For1Demo {
+    public static void main(String[] args) {
+        for(int i=1;i<5;i++){
+            System.out.print(i);
+        }
+    }
+}
